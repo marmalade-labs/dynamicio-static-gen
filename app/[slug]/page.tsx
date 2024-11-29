2d0d4821202da2fca65dd7d082217e5104d06e0b
Steve McClain <steve@marmalade.co>
2024-11-29 11:29:31 -0500

    Add a dummy data fetching function

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -4,7 +4,14 @@ export function generateStaticParams() {
 
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
+  const title = await fetchTitle(slug)
   return (
-    <div>{slug}</div>
+    <div>{title}</div>
   )
+}
+
+async function fetchTitle(slug: string) {
+  "use cache"
+  await new Promise(resolve => setTimeout(resolve, 5000))
+  return `Params: ${slug}`
 }
