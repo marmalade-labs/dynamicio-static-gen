5498753506ed8404135bddcc7c9503295e4e7fed
Steve McClain <steve@marmalade.co>
2024-11-29 13:57:12 -0500

    Simplify

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,17 +1,7 @@
-import { Metadata } from "next";
-import { headers } from "next/headers";
-import { Suspense, cache } from "react";
+import { cache } from "react";
 
 export function generateStaticParams() {
   return [{ slug: "static" }]
-}
-
-export async function generateMetadata({ params }: { params: Promise<{ slug: string }> }): Promise<Metadata> {
-  const { slug } = await params;
-  const title = await fetchTitle(slug)
-  return {
-    title,
-  }
 }
 
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
@@ -20,9 +10,6 @@ export default async function Page({ params }: { params: Promise<{ slug: string 
   return (
     <div className="flex flex-col gap-2">
       <div>{title}</div>
-      <Suspense fallback={<div>Accessing headers() ...</div>}>
-        <Dynamic />
-      </Suspense>
     </div>
   )
 }
@@ -32,8 +19,3 @@ const fetchTitle = cache(async function fetchTitle(slug: string) {
   await new Promise(resolve => setTimeout(resolve, 5000))
   return `Params: ${slug}`
 })
-
-async function Dynamic() {
-  const nextHeaders = await headers();
-  return <div>sec-ch-ua: {nextHeaders.get("sec-ch-ua")}</div>
-}
